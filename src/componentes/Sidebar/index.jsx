69da57f1aee50d49a7f87908718d0aa29d16a583
Diego <diego120689@gmail.com>
2026-07-01 08:19:13 -0300

    Inclusão imagens e início do CSS

--- src/componentes/Sidebar/index.jsx
+++ src/componentes/Sidebar/index.jsx
@@ -1,5 +1,8 @@
 import Logo from "./assets/Logo.svg";
 import Feed from "./assets/feed.svg";
+import Account from "./assets/account_circle.svg";
+import Info from "./assets/info.svg";
+import Logout from "./assets/logout.svg";
 
 export default function Sidebar() {
   return (
@@ -14,6 +17,18 @@ export default function Sidebar() {
             <a href="#">
               <img src={Feed} alt="" />
               <span> Feed </span>
+            </a>
+            <a href="#">
+              <img src={Account} alt="" />
+              <span> Perfil </span>
+            </a>
+            <a href="#">
+              <img src={Info} alt="" />
+              <span> Sobre nós </span>
+            </a>
+            <a href="#">
+              <img src={Logout} alt="" />
+              <span> Sair </span>
             </a>
           </li>
         </ul>
