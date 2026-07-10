54966bba346dd777a6140e6e8e64d64a5ba1a221
Diego <diego120689@gmail.com>
2026-07-09 22:43:25 -0300

    feat: adicionar componente Card com estilização e imagens

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import "./App.css";
 import BarraDePesquisa from "./componentes/BarraDePesquisa";
 import Filtro from "./componentes/Filtro";
 import Sidebar from "./componentes/Sidebar";
+/* import Ordenacao from "./componentes/Ordenacao"; */
+import Card from "./componentes/Card";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes")
+      .then(resposta => resposta.json())
+      .then(dados => setDados(dados))
+  }, []);
+  }
+
   return (
     <div className="container">
       <Sidebar />
       <div>
         <BarraDePesquisa />
         <Filtro />
+        <Card />
       </div>
     </div>
   );
