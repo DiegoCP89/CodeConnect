af2bf191c59f9934b21960f9235f71462d5ed9d9
Diego <diego120689@gmail.com>
2026-07-13 08:26:00 -0300

    fix: corrigir importação do componente Card e ajustar formatação do código

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import BarraDePesquisa from "./componentes/BarraDePesquisa";
 import Filtro from "./componentes/Filtro";
 import Sidebar from "./componentes/Sidebar";
 /* import Ordenacao from "./componentes/Ordenacao"; */
-import Card from "./componentes/Card";
+import Card from "./componentes/Cards";
 import { useEffect, useState } from "react";
 
 function App() {
   const [dados, setDados] = useState([]);
   useEffect(() => {
     fetch(
-      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes")
-      .then(resposta => resposta.json())
-      .then(dados => setDados(dados))
+      "https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes",
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => setDados(dados));
   }, []);
-  }
-
+  console.log(dados);
+  
   return (
     <div className="container">
       <Sidebar />
